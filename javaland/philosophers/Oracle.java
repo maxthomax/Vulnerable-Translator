36b212f4684d9fd59e37d1aa507f5743b33b9410
Thomas Herlea <thomas.herlea@verizonbusiness.com>
2010-05-04 00:11:46 +0200

    Replaced hardcoded question storage with a more flexible HashMap.
    Signed-off-by: Thomas Herlea <thomas.herlea@verizonbusiness.com>

--- javaland/philosophers/Oracle.java
+++ javaland/philosophers/Oracle.java
@@ -10,11 +10,24 @@ package javaland.philosophers;
 
 /**
  * @author Thomas Herlea
- * @date 2009-05-30
+ * @date 2010-05-03
  */
 public class Oracle {
-	private static String questionWeatherForecast = "21";
-	private static String questionIsPresidentCorrupt = "42";
+	private java.util.HashMap<String, String> knowledge = null;
+	private java.util.Set<String> questionCodes = null;
+
+	/**
+	 * The constructor.
+	 */
+	public Oracle() {
+		this.knowledge = new java.util.HashMap<String, String>(5,1);
+		this.knowledge.put("3", "The first odd prime number is lucky.");
+		this.knowledge.put("7", "The Magnificent created the world.");
+		this.knowledge.put("12", "The best prophecies are self-fulfilling.");
+		this.knowledge.put("21", "The weather will change.");
+		this.knowledge.put("42", "The current President is corrupt!");
+		this.questionCodes = this.knowledge.keySet();
+	}
 
 	/**
 	 * The program's entry point.
@@ -30,7 +43,7 @@ public class Oracle {
 	 * The program's main loop.
 	 */
 	private void run() {
-		java.io.BufferedReader reader =new java.io.BufferedReader(
+		java.io.BufferedReader reader = new java.io.BufferedReader(
 				new java.io.InputStreamReader(System.in));
 		String input = null;
 		boolean keepRunning = false;
@@ -58,13 +71,13 @@ public class Oracle {
 		if (pInput == null) {
 			return false;
 		}
-		if (pInput.contains(questionWeatherForecast)) {
-			System.out.println("The weather will improve, "+
-					"then it will worsen,... " +
-					"then it will improve again,...");
-		}
-		if (pInput.contains(questionIsPresidentCorrupt)) {
-			System.out.println("The current President is corrupt!");
+		String questionCode = null;
+		java.util.Iterator<String> questionsIterator = this.questionCodes.iterator();
+		while (questionsIterator.hasNext()) {
+			questionCode = questionsIterator.next();
+			if (pInput.contains(questionCode)) {
+				System.out.println(this.knowledge.get(questionCode));
+			}
 		}
 		return true;
 	}
