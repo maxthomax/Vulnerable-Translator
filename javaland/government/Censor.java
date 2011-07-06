6f21fe5d8a8e78ea71668bf6410fe070dc20a37e
Thomas Herlea <thomas.herlea+VulnerableTranslator@gmail.com>
2011-07-06 09:07:58 +0200

    Set license to GPLv3.

--- javaland/government/Censor.java
+++ javaland/government/Censor.java
@@ -1,3 +1,21 @@
+/*
+    Part of "Vulnerable Translator", which allows anyone to experience "hacking".
+    Copyright (C) 2009  Thomas Herlea
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * The censor of the questions. In an attempt to prevent citizens from asking
  * the Oracle the question whether the President is corrupt (code 42), this
